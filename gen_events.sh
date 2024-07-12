7d8f6c6a5b95342f92cd8c75899c957a7340812c
Alessio Marzullo <alessio.marzullo@txtgroup.com>
2024-07-12 09:35:18 +0200

    handling timestamp for apparatoù

--- gen_events.sh
+++ gen_events.sh
@@ -5,7 +5,7 @@
 # usage menu
 echo
 echo "---------------------- Usage ----------------------"
-echo -e "\n   bash $0\n\n    -tr < tratta > (ex. EUS)\n    -re < rete Entrata >\n    -pe < punto Entrata >\n    -ru < rete Uscita >\n    -pu < punto Uscita >\n    -rs < rete Svincolo >\n    -ps < punto Svincolo >\n    -sp < codice Service Provider >\n    -pl < plate number >\n"
+echo -e "\n   bash $0\n\n    -tr < tratta > (ex. EUS)\n    -re < rete Entrata >\n    -pe < punto Entrata >\n    -ru < rete Uscita >\n    -pu < punto Uscita >\n    -rs < rete Svincolo >\n    -ps < punto Svincolo >\n    -ap < tipo apparato (o-s) >\n    -sp < codice Service Provider >\n    -pl < plate number >\n"
 echo
 
 # Parsing degli OPTARGS
@@ -25,6 +25,8 @@ while [[ "$#" -gt 0 ]] ; do
 			 	shift 2;;
 		-ps) PUNTO_S="$2"
 			 	shift 2;;
+		-ap) APPARATO="$2"
+				shift 2;;
 		-sp) SERVICE_PROVIDER="$2"
 				shift 2;;
 		-pl) PLATE_NUMBER="$2"
@@ -67,27 +69,34 @@ fi
 sysdate=$(date +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
 timestamp_PAN=$(date +"%Y%m%d")
 
-id_temporale_ENTRATA=$(date -d '-4325 min' +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
-# sysdate -4315 to wait the SET timeout (for the Uscita event)
-id_temporale_USCITA=$(date -d '-4315 min' +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
-
-aperto_BOOL=false
 plate_f=${PLATE_NUMBER:0:2}
 plate_number=${PLATE_NUMBER:2:3}
 plate_l=${PLATE_NUMBER:5:2}
-PAN=$(generate_PAN $plate_number)
 
+if [ $APPARATO == 'o' ] ; then
+	echo -e "...creating Viaggio OBU $TRATTA\n"
+	time_old=4320
+else 
+	echo -e "...creating Viaggio SET $TRATTA\n"
+	PAN=$(generate_PAN $plate_number)
+	time_old=720
+fi
 
+id_temporale_ENTRATA=$(date -d "-$(expr $time_old - 4) min" +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
+# sysdate -4315 to wait the SET timeout (for the Uscita event)
+id_temporale_USCITA=$(date -d "-$(expr $time_old - 5) min" +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
+
+aperto_BOOL=false
 
 if [ $TRATTA == 'EUS' ] || [ $TRATTA == 'US' ] ; then
 	direction='998'
-	id_temporale_SVINCOLO=$(date -d '-4310 min' +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
+	id_temporale_SVINCOLO=$(date -d "-$(expr $time_old - 10) min" +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
 	if [ $TRATTA == 'US' ] ; then 
 		aperto_BOOL=true
 	fi
 elif [ $TRATTA == 'SEU' ] || [ $TRATTA == 'SU' ] ; then
 	direction='997'
-	id_temporale_SVINCOLO=$(date -d '-4330 min' +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
+	id_temporale_SVINCOLO=$(date -d "-$(expr $time_old - 3) min" +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
 	if [ $TRATTA == 'SU' ] ; then 
 		aperto_BOOL=true
 	fi
@@ -96,12 +105,17 @@ fi
 # extract event type from TRATTA
 length=${#TRATTA}
 i=0
+VIAGGIO_DIR="Viaggio-$TRATTA"
+mkdir $path_OUT_dir/$VIAGGIO_DIR
+path_VIAGGIO_dir=$path_OUT_dir/$VIAGGIO_DIR
+echo -e "...created '$VIAGGIO_DIR' at path: '$path_VIAGGIO_dir' \n"
+chmod 0777 "$path_VIAGGIO_dir"
 while [[ $i -lt $length ]] ; do 
 	t=${TRATTA:i:1}
     case $t in
         E)	
 			filename="entrataSET-$PUNTO_E.xml"
-			touch $path_OUT_dir/$filename
+			touch $path_VIAGGIO_dir/$filename
 			echo -e "...creating file '$filename'\n"
 			echo -e "rete: $RETE_E\npunto: $PUNTO_E\nidTemporale: $id_temporale_ENTRATA\nserviceProvider: $SERVICE_PROVIDER\npan: $PAN\n"
 			# to do scrivi nel file .xml
@@ -114,7 +128,7 @@ while [[ $i -lt $length ]] ; do
 			else 
 				filename="uscitaChiusoSET-$PUNTO_U.xml"
 			fi
-			touch $path_OUT_dir/$filename
+			touch $path_VIAGGIO_dir/$filename
 			echo -e "...creating file '$filename'\n"
 			echo -e "rete: $RETE_U\npunto: $PUNTO_U\nidTemporale: $id_temporale_USCITA\nserviceProvider: $SERVICE_PROVIDER\npan: $PAN\n"
 
@@ -127,7 +141,7 @@ while [[ $i -lt $length ]] ; do
 				nome_dir="DopoSET"
 			fi
 			filename="svincolo$nome_dir-$PUNTO_S.xml"
-			touch $path_OUT_dir/$filename
+			touch $path_VIAGGIO_dir/$filename
 			echo -e "...creating file '$filename'\n"
 			echo -e "rete: $RETE_S\npunto: $PUNTO_S\nidTemporale: $id_temporale_SVINCOLO\nserviceProvider: $SERVICE_PROVIDER\npan: $PAN\n"
 
