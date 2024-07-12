01b346964564542b3192ef68321ddc6f6d26303d
Alessio Marzullo <alessio.marzullo@txtgroup.com>
2024-07-12 16:43:28 +0200

    update README file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-TO DO
+gen_events.sh è uno script bash sviluppato per semplificare la creazione di Viaggi SET o OBU da dare in pasto a LEV.
+
+i parametri da dare in pasto allo script sono:
+    
+    -tr) permette di specificare la tipologia di Viaggio che si vuole creare (ex. 'EUS' acronimo di Entrata, Uscita e Svincolo)
+    
+    -re) rete dell'evento di Entrata
+    -pe) punto (o stazione) di Entrata
+    -ru) rete dell'evento di Uscita
+    -pu) punto (o stazione) di Uscita
+    -rs) rete dell'evento di Svincolo
+    -ps) punto (o stazione) di Svincolo
+
+    -ap) permette di specificare che tipologia di apparato viene utilizzata: SET (digitando 's') oppure OBU (digitando 'o')
+    -sp) permette di specificare il ServiceProvider di riferimento
+    -pl) permette di specificare la targa del veicolo (ex. AM000AM)
+
+Immaginiamo di voler lanciare il seguente comando : bash gen_events.sh -tr EUS -re 1 -pe 411 -ru 1 -pu 414 -rs 34 -ps 428 -pl AM001AM -sp 151 -ap s...
+
+Lo script crea, se non esiste già, una cartella 'OUT_DIR_EVENTS' al cui interno creerà, ad ogni run, un'ulteriore cartella contenente a sua volta i file da dare in pasto a LEV (tutte le cartelle saranno rinominate nel seguente modo: 'Viaggio<tipo_apparato>-<tratta>.xml'). Ogni file al suo interno sarà rinominato nel seguente modo: '<tipo_di_evento><tipo_apparato>-<stazione>.xml' in modo da essere facilmente riconoscibili.
+
+Lo script è pensato per automatizzare il più possibile la preparazione degli eventi, quindi:
+-   in caso di viaggio SET, genera un PAN in automatico seguendo questo pattern: '<YYYYMMDD>000..00<numero_targa>F'
+-   in caso di Viaggio OBU invece userà 'sysdate' prendendo in considerazione solo la parte di secondi e millisecondi (ex. 56002)
+-   sia in caso di SET che OBU, TUTTI gli idTemporali sono generati in maniera tale da attendere pochi minuti affinché LEV certifichi il viaggio risultante
+-   dando in input la tratta, lo script riconosce se l'evento è di Sistema Aperto o Chiuso, se è uno svincoloPrima o Dopo e la sua direzione (997 o 998)
+
+una volta terminato il run, basterà eseguire il drag and drop dei file nelle code JMS dedicate.
+
--- gen_events.sh
+++ gen_events.sh
@@ -62,7 +62,7 @@ else
 fi
 
 # vars declaration
-sysdate=$(date +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
+sysdate=$(date +"%Y-%m-%dT%H:%M:%S.%3N+02:00")
 timestamp_PAN=$(date +"%Y%m%d")
 aperto_BOOL=false
 plate_f=${PLATE_NUMBER:0:2}
@@ -83,29 +83,25 @@ else
 	time_old=720
 fi
 
-id_temporale_ENTRATA=$(date -d "-$(expr $time_old - 4) min" +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
-id_temporale_USCITA=$(date -d "-$(expr $time_old - 5) min" +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
+id_temporale_ENTRATA=$(date -d "-$(expr $time_old - 4) min" +"%Y-%m-%dT%H:%M:%S.%3N+02:00")
+id_temporale_USCITA=$(date -d "-$(expr $time_old - 5) min" +"%Y-%m-%dT%H:%M:%S.%3N+02:00")
 
 if [ $TRATTA == 'EUS' ] || [ $TRATTA == 'US' ] ; then
 	direction='998'
-	id_temporale_SVINCOLO=$(date -d "-$(expr $time_old - 10) min" +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
+	id_temporale_SVINCOLO=$(date -d "-$(expr $time_old - 10) min" +"%Y-%m-%dT%H:%M:%S.%3N+02:00")
 	if [ $TRATTA == 'US' ] ; then 
 		aperto_BOOL=true
 	fi
 elif [ $TRATTA == 'SEU' ] || [ $TRATTA == 'SU' ] ; then
 	direction='997'
-	id_temporale_SVINCOLO=$(date -d "-$(expr $time_old - 3) min" +"%Y-%m-%dT%H:%M.%S.%3N+02:00")
+	id_temporale_SVINCOLO=$(date -d "-$(expr $time_old - 3) min" +"%Y-%m-%dT%H:%M:%S.%3N+02:00")
 	if [ $TRATTA == 'SU' ] ; then 
 		aperto_BOOL=true
 	fi
 fi
 
-# extract event type from TRATTA
-length=${#TRATTA}
-i=0
 VIAGGIO_DIR="Viaggio$type_viaggio-$TRATTA"
 path_VIAGGIO_dir=$path_OUT_dir/$VIAGGIO_DIR
-
 
 # cancellare a fine sviluppo da qui a...
 if [ -d $path_VIAGGIO_dir ] ; then 
@@ -113,11 +109,14 @@ if [ -d $path_VIAGGIO_dir ] ; then
 fi
 # fino a qui 
 
-
 mkdir $path_OUT_dir/$VIAGGIO_DIR
 path_VIAGGIO_dir=$path_OUT_dir/$VIAGGIO_DIR
 echo -e "...created folder '$VIAGGIO_DIR' at path: '$path_VIAGGIO_dir' \n"
 chmod 0777 "$path_VIAGGIO_dir"
+
+# extract event type from TRATTA
+length=${#TRATTA}
+i=0
 while [[ $i -lt $length ]] ; do 
 	t=${TRATTA:i:1}
     case $t in
@@ -127,7 +126,31 @@ while [[ $i -lt $length ]] ; do
 			echo -e "...creating file '$filename'\n"
 			echo -e "rete: $RETE_E\npunto: $PUNTO_E\nidTemporale: $id_temporale_ENTRATA\nserviceProvider: $SERVICE_PROVIDER\n$type_viaggio: $APPARATO \n"
 			# to do scrivi nel file .xml
-
+cat << EOF > "$path_VIAGGIO_dir/$filename"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<ns0:evento xmlns:ns0="http://transit.pr.auto.aitek.it/messages">
+    <tipoEvento cod="E" />
+    <idSpaziale periferica="62" progrMsg="1" corsia="0" dirMarcia="1" tipoPeriferica="P" rete="${RETE_E}" punto="${PUNTO_E}" />
+    <idTemporale>${id_temporale_ENTRATA}</idTemporale>
+    <infoVeicolo classe="10">
+        <targaAnt nomeFile="targaAnt.jpg" affid="9" nazione="IT">${PLATE_NUMBER}</targaAnt>
+        <targaPost nomeFile="targaPost.jpg" affid="9" nazione="IT">${PLATE_NUMBER}</targaPost>
+        <targaRif nazione="IT">${PLATE_NUMBER}</targaRif>
+EOF
+if [ $type_viaggio == 'SET' ] ; then
+cat << EOF >> "$path_VIAGGIO_dir/$filename"	
+		<SET CodiceIssuer="${SERVICE_PROVIDER}" PAN="${APPARATO}" nazione="IT" EFCContextMark="604006001D09"/>
+EOF
+else
+cat << EOF >> "$path_VIAGGIO_dir/$filename"	
+		<OBU>${APPARATO}</OBU>
+EOF
+fi
+cat << EOF >> "$path_VIAGGIO_dir/$filename"
+	</infoVeicolo>
+    <reg dataOraMittente="${sysdate}" />
+</ns0:evento>
+EOF
 			((i++));;
 
 		U)	
@@ -140,11 +163,37 @@ while [[ $i -lt $length ]] ; do
 			echo -e "...creating file '$filename'\n"
 			echo -e "rete: $RETE_U\npunto: $PUNTO_U\nidTemporale: $id_temporale_USCITA\nserviceProvider: $SERVICE_PROVIDER\n$type_viaggio: $APPARATO \n"
 
+cat << EOF > "$path_VIAGGIO_dir/$filename"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<ns0:evento xmlns:ns0="http://transit.pr.auto.aitek.it/messages">
+    <tipoEvento cod="U" />
+    <idSpaziale periferica="62" progrMsg="4" corsia="0" dirMarcia="1" tipoPeriferica="P" rete="${RETE_U}" punto="${PUNTO_U}" />
+    <idTemporale>${id_temporale_USCITA}</idTemporale>
+    <infoVeicolo classe="10">
+        <targaAnt nomeFile="targaAnt.jpg" affid="9" nazione="IT">${PLATE_NUMBER}</targaAnt>
+        <targaPost nomeFile="targaPost.jpg" affid="9" nazione="IT">${PLATE_NUMBER}</targaPost>  
+        <targaRif nazione="IT">${PLATE_NUMBER}</targaRif>
+EOF
+if [ $type_viaggio == 'SET' ] ; then
+cat << EOF >> "$path_VIAGGIO_dir/$filename"	
+		<SET CodiceIssuer="${SERVICE_PROVIDER}" PAN="${APPARATO}" nazione="IT" EFCContextMark="604006001D09"/>
+EOF
+else
+cat << EOF >> "$path_VIAGGIO_dir/$filename"	
+		<OBU>${APPARATO}</OBU>
+EOF
+fi
+cat << EOF >> "$path_VIAGGIO_dir/$filename"
+    </infoVeicolo>
+    <idViaggio mezzoPagamento="TL" />
+    <reg dataOraMittente="${sysdate}" />
+</ns0:evento>
+EOF
 			((i++));;
 
 		S)
 			if [ $direction == '998' ] ; then
-				nome_dir="Dopo$type_viaggio" # to do aggiungere anche caso OBU
+				nome_dir="Dopo$type_viaggio"
 			else
 				nome_dir="Prima$type_viaggio"
 			fi
@@ -153,7 +202,34 @@ while [[ $i -lt $length ]] ; do
 			echo -e "...creating file '$filename'\n"
 			echo -e "rete: $RETE_S\npunto: $PUNTO_S\nidTemporale: $id_temporale_SVINCOLO\nserviceProvider: $SERVICE_PROVIDER\n$type_viaggio: $APPARATO \n"
 
+cat << EOF > "$path_VIAGGIO_dir/$filename"
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<ns2:evento xmlns:ns2="http://transit.pr.auto.aitek.it/messages">
+    <tipoEvento cod="S"/>
+    <idSpaziale corsia="1" dirMarcia="1" periferica="1" progrMsg="17621" punto="${PUNTO_S}" rete="${RETE_S}" tipoPeriferica="B"/>
+    <idTemporale>${id_temporale_SVINCOLO}</idTemporale>
+    <infoVeicolo>
+EOF
+if [ $type_viaggio == 'SET' ] ; then
+cat << EOF >> "$path_VIAGGIO_dir/$filename"	
+		<SET CodiceIssuer="${SERVICE_PROVIDER}" PAN="${APPARATO}" nazione="IT" EFCContextMark="604006001D09"/>
+EOF
+else
+cat << EOF >> "$path_VIAGGIO_dir/$filename"	
+		<OBU>${APPARATO}</OBU>
+EOF
+fi
+cat << EOF >> "$path_VIAGGIO_dir/$filename"
+    </infoVeicolo>
+	<datiEntrata>
+		<stazione rete="${RETE_S}" punto="${direction}"/>
+	</datiEntrata>
+    <reg dataOraMittente="${sysdate}"/>
+</ns2:evento>
+EOF
 			((i++));;
 		
     esac
 done
+
+echo -e "...all files are present at path: '$path_VIAGGIO_dir' \n"
